c970ad5f67ffb2ab3c15e681d9dd27fcc4c159a9
Dejavu75 <diego.zacur@dvsoft.com.ar>
2024-03-17 18:35:04 -0300

    Test 2

--- lib/sol_backoffice_api.dart
+++ lib/sol_backoffice_api.dart
@@ -4,5 +4,6 @@
 library;
 
 export 'src/sol_backoffice_api_base.dart';
+export 'src/schema/sch_configuraciones.dart';
 
 // TODO: Export any libraries intended for clients of this package.
--- lib/src/sol_backoffice_api_base.dart
+++ lib/src/sol_backoffice_api_base.dart
@@ -1,10 +1,3 @@
 import 'package:sol_backoffice_api/src/models/mod_configuraciones.dart';
 
-/// Checks if you are awesome. Spoiler: you are.
-class Awesome {
-  bool get isAwesome => true;
-}
-
-class ModConfiguraciones extends ModConfiguracionesBase {
-  ModConfiguraciones() : super();
-}
+class ModConfiguraciones extends ModConfiguracionesBase {}
