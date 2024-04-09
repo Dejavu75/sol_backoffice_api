ad0f2e84e0501c175aabf988a10b625ddb9aef1d
Dejavu75 <diego.zacur@dvsoft.com.ar>
2024-04-09 10:33:17 -0300

    Export

--- lib/sol_backoffice_api.dart
+++ lib/sol_backoffice_api.dart
@@ -7,8 +7,10 @@ export 'src/sol_backoffice_api_base.dart';
 export 'src/schema/sch_configuraciones.dart';
 export 'src/schema/sch_sistemas.dart';
 export 'src/schema/sch_backups.dart';
+export 'src/schema/sch_actualizaciones.dart';
 
 export 'src/controllers/con_configuraciones.dart';
 export 'src/controllers/con_sistemas.dart';
 export 'src/controllers/con_backups.dart';
+export 'src/controllers/con_actualizaciones.dart';
 // TODO: Export any libraries intended for clients of this package.
