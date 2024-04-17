cc41cb5aff9548c5f60e0bd71f63ce16961f11ad
Dejavu75 <diego.zacur@dvsoft.com.ar>
2024-04-17 17:11:20 -0300

    Flutter Quitado

--- lib/src/models/localdata/mod_configuraciones.dart
+++ lib/src/models/localdata/mod_configuraciones.dart
@@ -1,19 +1,29 @@
 import 'dart:convert';
 import 'package:sol_backoffice_api/src/schema/sch_configuraciones.dart';
 import 'dart:io';
-import 'package:flutter/services.dart';
 
 class ModConfiguracionesBase {
-  Future<SchConfiguraciones> obtenerConfiguraciones() async {
+  Future<SchConfiguraciones> obtenerConfiguraciones(
+      [String configString = ""]) async {
     String raw = '';
     try {
       // ignore: prefer_typing_uninitialized_variables
       var configData;
-      try {
-        final file = File('config.json');
-        raw = await file.readAsString();
-      } catch (e) {
-        raw = await rootBundle.loadString('config.json');
+      if (configString != "") {
+        raw = configString;
+      } else {
+        try {
+          final file = File('config.json');
+          raw = await file.readAsString();
+        } catch (e) {
+          return SchConfiguraciones(
+              conexion_actual: 0,
+              url: '',
+              nombre: '',
+              conexiones: [],
+              usuario: '',
+              apikey: '');
+        }
       }
       configData = json.decode(raw);
       SchConfiguraciones config2 = SchConfiguraciones.fromJson(configData);
